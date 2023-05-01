444e758f13a27031d1247c15182291777c28012f
Mohammedsalman14 <mohamedsalman140@gmail.com>
2023-05-01 17:59:11 +0530

    A Day with Buffer

--- Buffer/main.js
+++ Buffer/main.js
@@ -1,2 +1,15 @@
 const buffer=Buffer.alloc(8);
-console.log(buffer);
+
+buffer.write("st",'utf-8');
+
+console.log(buffer.toJSON());
+console.log(`length: `,buffer.length);
+
+const buffer2=Buffer.from("string",'utf-8');
+console.log(buffer2.toJSON());
+
+const buffer3=Buffer.from([97,66,67],"dec");
+console.log(buffer3.toString('utf-8'));
+
+const buffer4=Buffer.from("E0AEAEE0AF81",'hex');
+console.log(buffer4.toString('utf-8'));
